2a401395548e8799a531bcd4be6eb511fa7f2e72
Filip Cichowski <filip.cichowski.art@gmail.com>
2024-01-26 13:04:32 +0100

    Styled first page

--- SubjectMatterExpertFrontend/src/app/core/register-form/register-form.service.ts
+++ SubjectMatterExpertFrontend/src/app/core/register-form/register-form.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RegisterFormService {
-  private registrationForm: FormGroup;
+  private readonly registrationForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
     this.registrationForm = this.fb.group({
@@ -15,15 +15,44 @@ export class RegisterFormService {
         [
           Validators.required,
           Validators.minLength(8),
+          this.validatePasswordStrength()
         ],
       ],
-      repeatPassword: ['', [Validators.required, Validators.minLength(8)]],
+      repeatPassword: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+        ],
+      ],
       name: ['', Validators.required],
       surname: ['', Validators.required],
+    }, {
+      validators: this.passwordsMatchValidator
     });
   }
 
   getForm(): FormGroup {
     return this.registrationForm;
+  }
+
+  private passwordsMatchValidator = (control: FormGroup): { [key: string]: boolean } | null => {
+    const password = control.get('password');
+    const repeatPassword = control.get('repeatPassword');
+
+    return password && repeatPassword && password.value !== repeatPassword.value ? { 'passwordsDoNotMatch': true } : null;
+  };
+
+  private validatePasswordStrength(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: boolean } | null => {
+      const password = control.value;
+      if (password) {
+        const strongRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/;
+        if (!strongRegex.test(password)) {
+          return { 'weakPassword': true };
+        }
+      }
+      return null;
+    };
   }
 }
