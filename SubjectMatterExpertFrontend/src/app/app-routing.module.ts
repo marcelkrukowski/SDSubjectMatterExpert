54533d174223f10411322012b32c0bf6d9569403
Henish Nobeen <henish.nobeen@sdworx.com>
2024-01-30 12:42:08 +0400

    Added forgotPage frontend

--- SubjectMatterExpertFrontend/src/app/app-routing.module.ts
+++ SubjectMatterExpertFrontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotPasswordComponent } from './core/components/forgotPassword-page/forgot-password/forgot-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'forgotPassword', component : ForgotPasswordComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- SubjectMatterExpertFrontend/src/app/app.module.ts
+++ SubjectMatterExpertFrontend/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ForgotPasswordComponent } from './core/components/forgotPassword-page/forgot-password/forgot-password.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
