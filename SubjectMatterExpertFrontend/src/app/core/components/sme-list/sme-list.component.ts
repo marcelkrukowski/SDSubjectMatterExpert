6a8a2752386c860e6b952854a0e77262474247cf
Filip Cichowski <filip.cichowski.art@gmail.com>
2024-02-01 08:51:07 +0100

    List is fully responsive & styled now. Filter page is to be added.

--- SubjectMatterExpertFrontend/src/app/core/components/sme-list/sme-list.component.ts
+++ SubjectMatterExpertFrontend/src/app/core/components/sme-list/sme-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { SME } from './sme.model';
 
 @Component({
@@ -20,12 +20,142 @@ export class SmeListComponent implements OnInit {
       expertise: 'angular JS',
       availability: 'Available'
     },
-    // ... other SMEs
+    {
+      name: 'Jacek Nowak',
+      title: 'Pro Engineer',
+      expertise: 'angular JS',
+      availability: 'Available'
+    },
+    {
+      name: 'Jacek Nowak',
+      title: 'Pro Engineer',
+      expertise: 'angular JS',
+      availability: 'Available'
+    },
+    {
+      name: 'Jacek Nowak',
+      title: 'Pro Engineer',
+      expertise: 'angular JS',
+      availability: 'Available'
+    },
+    {
+      name: 'Jacek Nowak',
+      title: 'Pro Engineer',
+      expertise: 'angular JS',
+      availability: 'Available'
+    },
+    {
+      name: 'Jacek Nowak',
+      title: 'Pro Engineer',
+      expertise: 'angular JS',
+      availability: 'Available'
+    },
+    {
+      name: 'Jacek Nowak',
+      title: 'Pro Engineer',
+      expertise: 'angular JS',
+      availability: 'Available'
+    },
+    {
+      name: 'Jacek Nowak',
+      title: 'Pro Engineer',
+      expertise: 'angular JS',
+      availability: 'Available'
+    },
+    {
+      name: 'Jacek Nowak',
+      title: 'Pro Engineer',
+      expertise: 'angular JS',
+      availability: 'Available'
+    },
+    {
+      name: 'Jacek Nowak',
+      title: 'Pro Engineer',
+      expertise: 'angular JS',
+      availability: 'Available'
+    },
+    {
+      name: 'Jacek Nowak',
+      title: 'Pro Engineer',
+      expertise: 'angular JS',
+      availability: 'Available'
+    },
+    {
+      name: 'Jacek Nowak',
+      title: 'Pro Engineer',
+      expertise: 'angular JS',
+      availability: 'Available'
+    },
+    {
+      name: 'Jacek Nowak',
+      title: 'Pro Engineer',
+      expertise: 'angular JS',
+      availability: 'Available'
+    },
   ];
 
-  constructor() { }
+  paginatedSmeList: SME[] = [];
+  currentPage = 0;
+  pageSize = 3; // Initial value, will be recalculated
+  totalPages = 0;
+  itemHeight = 280; // Height of each item in pixels
+  isPaginationEnabled = true;
 
-  ngOnInit(): void {
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.adjustPageSize();
+    this.adjustPagination();
   }
 
+  ngOnInit() {
+    this.adjustPageSize();
+    this.adjustPagination();
+  }
+
+  adjustPageSize() {
+    const viewportWidth = window.innerWidth;
+    const viewportHeight = window.innerHeight;
+
+    if (viewportWidth < 768) {
+      this.isPaginationEnabled = false;
+      this.pageSize = this.smeList.length; // Set pageSize to full list length
+    } else {
+      this.pageSize = Math.floor(viewportHeight / this.itemHeight);
+      this.isPaginationEnabled = this.pageSize < this.smeList.length;
+    }
+  }
+
+  adjustPagination() {
+    this.totalPages = Math.ceil(this.smeList.length / this.pageSize);
+
+    if (this.currentPage >= this.totalPages) {
+      this.currentPage = Math.max(this.totalPages - 1, 0);
+    }
+
+    if (this.isPaginationEnabled) {
+      this.paginate();
+    } else {
+      this.paginatedSmeList = this.smeList; // Show all items if pagination is disabled
+    }
+  }
+
+  paginate() {
+    const startIndex = this.currentPage * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.paginatedSmeList = this.smeList.slice(startIndex, endIndex);
+  }
+
+  previousPage() {
+    if (this.currentPage > 0) {
+      this.currentPage--;
+      this.paginate();
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages - 1) {
+      this.currentPage++;
+      this.paginate();
+    }
+  }
 }
