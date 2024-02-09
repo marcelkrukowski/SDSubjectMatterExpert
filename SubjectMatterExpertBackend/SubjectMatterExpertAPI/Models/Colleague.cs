670a3a5ac98e3cc5bf78812761956e1d092c3293
Henish Nobeen <henish.nobeen@sdworx.com>
2024-02-09 14:11:35 +0400

    Fixed string to int error on colleagues.cs

--- SubjectMatterExpertBackend/SubjectMatterExpertAPI/Models/Colleague.cs
+++ SubjectMatterExpertBackend/SubjectMatterExpertAPI/Models/Colleague.cs
@@ -6,7 +6,7 @@ namespace SubjectMatterExpertAPI.Models
     [Table("Colleagues")]
     public class Colleague
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string first_name { get; set; }
         public string last_name { get; set; }
         public int SessionId { get; set; }
