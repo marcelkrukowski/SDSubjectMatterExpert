a1c1181172db64a94cce46abc7d84e73282d3674
Marcel Krukowski <marcelmarek.krukowski@gmail.com>
2024-02-23 14:13:38 +0100

    small fixes in request

--- SubjectMatterExpertBackend/SubjectMatterExpertAPI/DTOs/UserWithPendingRequestDto.cs
+++ SubjectMatterExpertBackend/SubjectMatterExpertAPI/DTOs/UserWithPendingRequestDto.cs
@@ -5,7 +5,7 @@
         public int RequestId { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
-        public string Fristname { get; set; }
+        public string Firstname { get; set; }
         public string Lastname { get; set; }  
         public string Location { get; set; }
         public List<LanguageDto> Languages { get; set; }
