41ecdc1badd277fbfd2146346d1b6b6d47bf46b5
Koushal Oodith <koushal.oodith@sdworx.com>
2024-02-25 13:31:17 +0400

    update badrequest in sessionControllers

--- SubjectMatterExpertBackend/SubjectMatterExpertAPI/Controllers/SessionController.cs
+++ SubjectMatterExpertBackend/SubjectMatterExpertAPI/Controllers/SessionController.cs
@@ -87,12 +87,12 @@ namespace SubjectMatterExpertAPI.Controllers
             var isUserInSmeRole = await _userManager.IsInRoleAsync(user, "SME");
             if (!isUserInSmeRole)
             {
-                return BadRequest("AHAHAHAHHAHAHA TEST");
+                return BadRequest("");
             }
 
             if  (user.Sessions.All(s => s.Id != session.Id))
             {
-                return BadRequest("Test 2");
+                return BadRequest("");
             }
 
             session.Topic = updatedSessionDto.Topic;
